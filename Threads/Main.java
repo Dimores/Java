831d6b5fbb40145f24bfd907df5fdd20539f07bc
Diego Morelo <87763559+Dimores@users.noreply.github.com>
2022-09-27 10:42:49 -0300

    Correção

--- Threads/Main.java
+++ Threads/Main.java
@@ -2,14 +2,16 @@ public class Main{
     
     public static void main(String[] args){
         
-        ThreadSimples t1 = new ThreadSimples("Thread1", 1);
-        ThreadSimples t2 = new ThreadSimples("Thread2", 2);
-        ThreadSimples t3 = new ThreadSimples("Thread3", 3);
-        ThreadSimples t4 = new ThreadSimples("Thread4", 4);
-        
+        Thread t1 = new Thread(
+            () -> System.out.println("Eu amo Threads!"));
         t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+        //ThreadSimples t2 = new ThreadSimples("Thread2", 2);
+        //ThreadSimples t3 = new ThreadSimples("Thread3", 3);
+        //ThreadSimples t4 = new ThreadSimples("Thread4", 4);
+        
+        //t1.start();
+        //t2.start();
+        //t3.start();
+        //t4.start();
     }
 }
